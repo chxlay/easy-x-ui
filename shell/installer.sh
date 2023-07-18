93668657c8c8858aead13a8781d30711bf1012b1
Alay <chxlay@aliyun.com>
2023-07-18 16:53:49 +0800

    初始化

--- shell/installer.sh
+++ shell/installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 安装 docker 环境
-sudo chmod +x ./docker-install.sh && ./docker-install.sh
+sudo chmod +x ./docker-installer.sh && ./docker-installer.sh
 
 # 给当前用户（非 root 用户）赋予 docker 的执行权限
 sudo usermod -aG docker $USER
 
 # 安装docker-compose 环境
-sudo chmod +x ./compose-install.sh && ./compose-install.sh
+sudo chmod +x ./compose-installer.sh && ./compose-installer.sh
 
 # 重启电脑
 sudo reboot
