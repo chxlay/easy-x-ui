6b1ecedf886f94e381c04f5d4ae6274bb23167e7
Alay <chxlay@aliyun.com>
2023-07-18 16:49:42 +0800

    初始化

--- shell/xui-installer.sh
+++ shell/xui-installer.sh
@@ -5,5 +5,7 @@ sudo mkdir -p /var/appdata
 # 拷贝 nginx 相关配置到挂载路径下
 sudo cp -r $PWD/../nginx /var/appdata/
 
+# 构建 x-ui 镜像
+docker-compose -f $PWD/../xui/docker/docker-compose.yml build easy-xui
 # 启动服务
 docker-compose -f $PWD/../xui/docker/docker-compose.yml up
