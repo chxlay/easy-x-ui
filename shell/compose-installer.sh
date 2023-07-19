d119fb25d174d363d6fc1e4f30b644e183670469
Alay <chxlay@aliyun.com>
2023-07-19 14:15:18 +0800

    初始化

--- shell/compose-installer.sh
+++ shell/compose-installer.sh
@@ -3,7 +3,7 @@
 # 下载 docker-compose 在 opt路径下执行
 sudo curl -SLO https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-x86_64
 
-sudo ./docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+sudo mv ./docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 
 # 4、赋予权限：
 sudo chmod +x /usr/local/bin/docker-compose
