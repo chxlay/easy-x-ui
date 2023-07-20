0ac454a13166ab7fd6920ffe6fa8b1224424b368
Alay <chxlay@aliyun.com>
2023-07-20 11:36:55 +0800

    初始化

--- shell/xui-installer.sh
+++ shell/xui-installer.sh
@@ -2,8 +2,11 @@
 
 # 创建应用路径
 sudo mkdir -p /var/appdata
-# 拷贝 nginx 相关配置到挂载路径下
+# 拷贝 nginx 相关配置到挂载路径下/var/appdata/
 sudo cp -r $PWD/../nginx /var/appdata/
+
+# 拷贝 x-ui配置文件到挂在的路径下
+sudo cp -r $PWD/../xui/config.json /var/appdata/x-ui/conf/
 
 # 构建 x-ui 镜像
 docker-compose -f $PWD/../xui/docker/docker-compose.yml build easy-xui
